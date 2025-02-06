989a4e53372ab08d4602421cded1ff508a81b556
HerrSpaten <78225081+DanielSpaten@users.noreply.github.com>
2025-02-06 10:10:50 +0100

    Update BitWriter.gd

--- BitWriter.gd
+++ BitWriter.gd
@@ -38,18 +38,18 @@ func write_bool_flag(value: bool) -> void:
 	write_bit(int(value))
 
 ## Writes the 'bits'-many bits of an integer to the Byte-Array
-func write_int(value: int, bits: int) -> void:
+func write_uint(value: int, bits: int) -> void:
 	for bit in bits:
 		write_bit(value & (1 << bit) != 0)
 
 ## Writes the 'bits'-amount of bits of a signed-integer to the Byte-Array
 func write_signed_int(value: int, bits: int) -> void:
-	write_int(abs(value), bits)
+	write_uint(abs(value), bits)
 	write_bool_flag(value < 0)
 
-## Writes a integer with the minimum amount of bytes possible (calculates on its own). [br]
+## Writes an unsigned integer with the minimum amount of bytes possible (calculates on its own). [br]
 ## Note that this is inefficient with integers > 16777215, as this has a custom bit-header
-func write_var_int(value: int) -> void:
+func write_var_uint(value: int) -> void:
 	var byteMask: int = 0b11111111 << 56
 	var bytesToWrite: int = 1
 	for intbyte in 8:
@@ -58,13 +58,13 @@ func write_var_int(value: int) -> void:
 			break
 		byteMask = byteMask >> 8
 	
-	write_int(bytesToWrite, 3)
-	write_int(value, bytesToWrite*8)
+	write_uint(bytesToWrite, 3)
+	write_uint(value, bytesToWrite*8)
 
 ## Writes a signed integer with the minimum amount of bytes possible (calculates on its own). [br]
 ## Note that this is inefficient with integers > 16777215, as this has a custom bit-header
-func write_signed_var_int(value: int) -> void:
-	write_var_int(abs(value))
+func write_var_int(value: int) -> void:
+	write_var_uint(abs(value))
 	write_bool_flag(value < 0)
 
 ## Returns the Byte-Array with the written bits inside of it. [br]
@@ -73,13 +73,13 @@ func get_byte_array() -> PackedByteArray:
 	if _bit_pointer != POINTER_RESET:
 		_write_to_new_byte()
 	
-	var byteArray: PackedByteArray = []
-	byteArray.resize(_data.size())
+	var byte_array: PackedByteArray = []
+	byte_array.resize(_data.size())
 	for i: int in _data.size():
-		byteArray.encode_s8(i, _data[i])
+		byte_array.encode_s8(i, _data[i])
 	_data.clear()
 	
-	return byteArray
+	return byte_array
 
 ## returns the amount of bits written. Useful for debugging
 func bits_written() -> int:
